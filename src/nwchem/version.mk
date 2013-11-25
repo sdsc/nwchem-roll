5dd42456cd16406a5aa6bac50859301fd1e87977
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 13:33:59 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/nwchem/version.mk
+++ src/nwchem/version.mk
@@ -1,3 +1,14 @@
-NAME    = nwchem_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= 6.3
-RELEASE	= 1
+NAME               = nwchem_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 6.3
+RELEASE            = 1
+PKGROOT            = /opt/nwchem
+
+SRC_SUBDIR         = nwchem
+
+SOURCE_NAME        = nwchem
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
