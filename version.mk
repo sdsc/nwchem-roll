16181aa37769232ec51007a1afb47983cc4571ea
Jerry Greenberg <jpg@comet-devel.local>
2015-03-10 16:57:45 -0700

    added commit sha to roll name

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = nwchem
-RELEASE  = 0
-COLOR    = orange
+ROLLNAME        = nwchem
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)
