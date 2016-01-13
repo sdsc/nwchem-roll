320c5c305e88a824b88079784c861084766ba619
Jerry Greenberg <jpg@sdsc.edu>
2016-01-13 15:24:42 -0800

    APS-241: new version: 6.6

--- src/nwchem/version.mk
+++ src/nwchem/version.mk
@@ -9,17 +9,17 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-nwchem
-VERSION        = 6.5
-RELEASE        = 2
+VERSION        = 6.6
+RELEASE        = 0
 PKGROOT        = /opt/nwchem
 
 SRC_SUBDIR     = nwchem
 
 SOURCE_NAME    = Nwchem
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION = 6.5.revision26243-src.2014-09-10
+SOURCE_VERSION = 6.6.revision27746-src.2015-10-20
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_DIR     = nwchem-$(VERSION)
 
 TAR_GZ_PKGS    = $(SOURCE_PKG)
 
