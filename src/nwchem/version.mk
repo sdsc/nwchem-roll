e3f51e955d6becdf3123a14df7260a68db8f0cf5
Jerry Greenberg <jpg@sdsc.edu>
2019-04-17 12:27:06 -0700

    APS-1006 remove obsolete packages

--- src/nwchem/version.mk
+++ src/nwchem/version.mk
@@ -28,16 +28,6 @@ SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = nwchem-$(VERSION)
 
-NWCHEM_PATCHES = Tddft_mxvec20 Tools_lib64 Config_libs66 Cosmo_meminit Sym_abelian Xccvs98 Dplot_tolrho Driver_smalleig Ga_argv Raman_displ Ga_defs Zgesvd Cosmo_dftprint Txs_gcc6 Gcc6_optfix Util_gnumakefile Util_getppn Gcc6_macs_optfix Notdir_fc Xatom_vdw Hfmke Cdfti2nw66
-
-$(NWCHEM_PATCHES):
-	$(eval PATCH_NAME  = $@)
-	$(eval $(PATCH_NAME)_SUFFIX = patch.gz)
-	$(eval $(PATCH_NAME)_PKG = $(PATCH_NAME).$($(PATCH_NAME)_SUFFIX))
-	$(eval PATCH_PKGS  += $($(PATCH_NAME)_PKG))
-	$(eval PATCHES  += $(PATCH_NAME).patch)
-	echo "PATCHES $(PATCHES)"
-
 GA_RELEASE_NO=5.6.3
 
 TAR_BZ2_PKGS    = $(SOURCE_PKG)
