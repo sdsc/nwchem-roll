790a19a9dd1bf780ed1d3339148cdbbc80efedfa
Jerry Greenberg <jpg@sdsc.edu>
2019-04-17 12:11:18 -0700

    APS-1006 add nwchem patches

--- src/nwchem/version.mk
+++ src/nwchem/version.mk
@@ -28,6 +28,16 @@ SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = nwchem-$(VERSION)
 
+NWCHEM_PATCHES = Tddft_mxvec20 Tools_lib64 Config_libs66 Cosmo_meminit Sym_abelian Xccvs98 Dplot_tolrho Driver_smalleig Ga_argv Raman_displ Ga_defs Zgesvd Cosmo_dftprint Txs_gcc6 Gcc6_optfix Util_gnumakefile Util_getppn Gcc6_macs_optfix Notdir_fc Xatom_vdw Hfmke Cdfti2nw66
+
+$(NWCHEM_PATCHES):
+	$(eval PATCH_NAME  = $@)
+	echo "PATCH NAME $(PATCH_NAME)"
+	$(eval $(PATCH_NAME)_SUFFIX = patch.gz)
+	$(eval $(PATCH_NAME)_PKG = $(PATCH_NAME).$($(PATCH_NAME)_SUFFIX))
+	echo "PACKAGE $($(PATCH_NAME)_PKG)"
+	$(eval PATCH_PKGS  += $($(PATCH_NAME)_PKG))
+
 GA_RELEASE_NO=5.6.3
 
 TAR_BZ2_PKGS    = $(SOURCE_PKG)
